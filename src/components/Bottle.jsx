1ce03a2bfcd4e27f698527deba73f2880dfcbf64
dipu-37 <razibul1105nstu@gmail.com>
2024-06-08 22:37:30 +0600

    finish task

--- src/components/Bottle.jsx
+++ src/components/Bottle.jsx
@@ -1,14 +1,15 @@
 
 import './bottle.css'
-const Bottle = ({bottle}) => {
+const Bottle = ({bottle, handleAddtocard}) => {
     const {name , img , price}=bottle;
-    console.log(bottle)
+    //console.log(bottle)
     return (
 
     <div className="bottle">
         <h3>Bottle:{name} </h3>
         <img src={img} alt="" />
         <p>price: {price}</p>
+        <button onClick={()=>handleAddtocard(bottle)}>Parches</button>
     </div>
         
        
--- src/components/Bottles.jsx
+++ src/components/Bottles.jsx
@@ -6,6 +6,7 @@ import './Bottles.css'
 
 const Bottles = () => {
     const [bottoles,setbottles]=useState([]);
+    const [card , setCard]= useState([])
 
 useEffect(()=>{
  fetch('bottles.json')
@@ -13,16 +14,23 @@ useEffect(()=>{
  .then(data =>setbottles(data))
 },[])
 
+const handleAddtocard = bottle =>{
+    // console.log(bottle)
+    // console.log('add bottle card')
+    const newcard = [...card, bottle];
+    setCard(newcard);
+}
 
     return (
         <div>
-        <h3>here</h3>
+        <h3>Bottals Available :{bottoles.length} </h3>
+        <h4>card : {card.length}</h4>
         <div className="bottle-container">
         
         {
             bottoles.map(bottle => <Bottle 
                 bottle={bottle} 
-                key={bottle.id}></Bottle>)
+                key={bottle.id} handleAddtocard={handleAddtocard}></Bottle>)
           }
        </div>
         </div>
